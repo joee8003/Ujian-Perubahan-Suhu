b7bc13dffec53243d5c41db6d24222242c2a45ff
joee8003 <gustam.wibisono@gmail.com>
2022-03-12 16:52:17 +0700

    first commit

---
+++ src/main/java/PerubahanSuhu.java
@@ -0,0 +1,9 @@
+
+
+public class PerubahanSuhu {
+	
+	public double testperubahanSuhu(double f, double expect) {
+		
+		return ((f-32)*5/9);
+	}
+}
---
+++ src/test/java/com/juaracoding/perubahansuhu/TestPerubahanSuhu.java
@@ -0,0 +1,67 @@
+package com.juaracoding.perubahansuhu;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.junit.runner.RunWith;
+import junitparams.FileParameters;
+import junitparams.JUnitParamsRunner;
+
+@RunWith(JUnitParamsRunner.class)
+public class TestPerubahanSuhu {
+	
+	PerubahanSuhu perubahanSuhu;
+	
+	@Before
+	public void setUp() throws Exception {
+		perubahanSuhu = new PerubahanSuhu();
+		
+	}
+	@Test
+	@FileParameters("src/test/resources/PerubahanSuhu")
+	
+	public void testPerubahanSuhu(double f, double expect) {
+	
+	double actual = perubahanSuhu.testperubahanSuhu(f, expect);
+	
+	assertEquals(expect, actual, 0.0);
+	
+	}
+	@Test
+	public void testVolumeKolamRenangBukanAngka() {
+		
+		String fBukanAngka = "D";
+		
+		double convert = Double.parseDouble(fBukanAngka);
+		
+		double actual = perubahanSuhu.testperubahanSuhu(0,convert);		
+		
+		String expect = "D";
+		
+		assertEquals(Double.parseDouble(expect), actual, 0.0);
+		
+	}
+	
+	@Test
+	public void TestPerubahanSuhuAngkaNegatif() {
+	
+	double f = -5;
+	
+	double actual = perubahanSuhu.testperubahanSuhu(f, f);
+	
+	assertEquals(5, actual, 0.0);
+	
+	}
+	
+	@Test
+	public void testPerubahanSuhuNull() {
+	
+	double f = 0;
+	
+	Double fNull = null;
+		
+	
+	}
+}
